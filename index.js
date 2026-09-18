9ce20316a36ef96220fec2f1a7f135e5509cfe6e
Mosin Attar <mosinattar2604@gmail.com>
2026-09-18 12:19:24 +0530

    Add JavaScript food list using push method

---
+++ index.js
@@ -0,0 +1,17 @@
+// Food List using JavaScript push()
+
+let foodList = [];
+
+foodList.push("Pizza");
+foodList.push("Burger");
+foodList.push("Biryani");
+foodList.push("Dosa");
+foodList.push("Idli");
+foodList.push("Paneer Tikka");
+foodList.push("Fried Rice");
+foodList.push("Noodles");
+foodList.push("Sandwich");
+foodList.push("Pasta");
+
+console.log("Food List:");
+console.log(foodList);
